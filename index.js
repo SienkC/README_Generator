3ea95992d8c5a4f9641d5b635bdc4e540e7c69a9
Courtney Sienkiewich <sienkiewichc@gmail.com>
2023-07-23 13:24:18 -0400

    Added prompts for user

--- index.js
+++ index.js
@@ -1,31 +1,32 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
+const genMarkdwn = require('./utils/generateMarkdown.js');
 
-// TODO: Create an array of questions for user input
+// Array of questions for user input
 const questions = [
     {
         type:'input',
         message:'What is the title of your application?',
-        choices:'none',
+        choices:null,
         name:'title'
     },
     {
         type:'input',
         message:'Write a brief description of your application.',
-        choices:'none',
+        choices:null,
         name:'description'
     },
     {
         type:'input',
         message:'Write a brief summary of how to instal your application.',
-        choices:'none',
+        choices:null,
         name:'installation'
     },
     {
         type:'input',
         message:'Write a brief summary of how to use your application.',
-        choices:'none',
+        choices:null,
         name:'usage'
     },
     {
@@ -37,25 +38,25 @@ const questions = [
     {
         type:'input',
         message:'Write a brief summary of how others can contrubute to your application.',
-        choices:'none',
+        choices:null,
         name:'contributing'
     },
     {
         type:'input',
         message:'Write a how others can test your application.',
-        choices:'none',
+        choices:null,
         name:'tests'
     },
     {
         type:'input',
         message:'Add your github username.',
-        choices:'none',
+        choices:null,
         name:'github'
     },
     {
         type:'input',
         message:'Add your email.',
-        choices:'none',
+        choices:null,
         name:'email'
     }
 ];
@@ -65,37 +66,46 @@ function writeToFile(fileName, data) {
 
 }
 
-// TODO: Create a function to initialize app
 function init() {
-    inquirer
-    .prompt([
-        {
-            type: 'input',
-            message: 'What is your user name?',
-            name: 'name',
-        },
-        {
-            type: 'checkbox',
-            message: 'What languages do you know?',
-            choices: ['English', 'French', 'Japanese', 'Italian', 'Spanish'],
-            name: 'languages',
-        },
-        {
-            type: 'list',
-            message: 'What is your preferred method of communication?',
-            choices: ['Email', 'Phone', 'Fax'],
-            name: 'communication',
+    // Ask user questions in terminal
+    questions.forEach(question => {
+        if(question.choices) {
+            inquirer
+            .prompt(
+                {
+                    type: question.type,
+                    message: question.message,
+                    choices: question.choices,
+                    name: question.name
+                }
+            )
+            .then()
         }
-    ]).then((response) =>
-        // adds data as text
-        // fs.appendFile('answers.txt', `Name: ${response.name}\nLanguages: ${response.languages}\nCommunication: ${response.communication}\n`, (err) =>
-        //     err ? console.error(err) : console.log('Success!')
-        // )
+        else {
+            inquirer
+            .prompt(
+                {
+                    type: question.type,
+                    message: question.message,
+                    name: question.name
+                }
+            )
+            .then()
+        }
+        
+    });
 
-        // or this creates the info as a json style item
-        fs.appendFile('answers.txt', JSON.stringify(response, null, '\t'), (err) =>
-        err ? console.error(err) : console.log('Success!'))
-    );
+    
+    // ]).then((response) =>
+    //     // adds data as text
+    //     // fs.appendFile('answers.txt', `Name: ${response.name}\nLanguages: ${response.languages}\nCommunication: ${response.communication}\n`, (err) =>
+    //     //     err ? console.error(err) : console.log('Success!')
+    //     // )
+
+    //     // or this creates the info as a json style item
+    //     fs.appendFile('answers.txt', JSON.stringify(response, null, '\t'), (err) =>
+    //     err ? console.error(err) : console.log('Success!'))
+    // );
 }
 
 // Function call to initialize app
